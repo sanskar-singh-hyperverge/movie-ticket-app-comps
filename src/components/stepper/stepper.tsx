33e6838d14b526a9a9636a3cfd6dbd76932ef0f8
Gaurav Sharma <gaurav021201@gmail.com>
2025-01-10 13:58:18 +0530

    fix stepper

--- src/components/stepper/stepper.tsx
+++ src/components/stepper/stepper.tsx
@@ -3,26 +3,27 @@ import React from "react";
 export type StepperProps = {
   currentStep: number;
   totalStep: number;
+  className?: string;
 };
 
-const Stepper = ({ currentStep, totalStep }: StepperProps) => {
+const Stepper = ({ currentStep, totalStep, className }: StepperProps) => {
   const steps = Array.from({ length: totalStep }, (_, index) => index + 1);
 
   return (
-    <div className="flex items-center">
+    <div className={"flex items-center " + className}>
       {steps.map((step, index) => (
         <React.Fragment key={index}>
           <div
-            className={`w-10 h-10 flex items-center justify-center rounded-full ${
+            className={`flex w-7 h-7 items-center justify-center rounded-full ${
               step <= currentStep
                 ? "bg-purple-500 text-white"
                 : "border-2 border-purple-500 text-purple-500"
             }`}
           >
             {step < currentStep ? (
-              <span className="text-white text-xl">✓</span>
+              <span className="text-white text-xs">✓</span>
             ) : (
-              <span className="text-xl">{step}</span>
+              <span className="text-xl text-xs">{step}</span>
             )}
           </div>
           {index < totalStep - 1 && (
